68fa475a1bf535015682f9dd2ce5fee0d04178cb
n0931371122 <n0931371122@gmail.com>
2021-09-06 14:30:06 +0800

    add 404.php

---
+++ 404.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="zh-Hant">
+
+<head>
+    <?php include "header-title.php"; ?>
+
+</head>
+
+<body>
+    <div class="wp">
+        <!-- main ======================================================================================= -->
+        <main class="page-404 text-center flex-center les1 px-4">
+            <div class="pb-5 mb-md-5">
+                <h1 class="font-280 roboto font-weight-bold text-00A1E0 opacity05 lh085" data-aos="fade-up">404</h1>
+                <p class="font-50 font-weight-bold mb-2" data-aos="fade-up" data-aos-delay="50">找不到頁面</p>
+                <p class="font-24" data-aos="fade-up" data-aos-delay="100">很抱歉！您所找的頁面不存在或輸入錯誤的網址。</p>
+                <a class="flex-center font-18 bg-00A1E0 text-white w230 h70 rounded-50 mx-auto mt-5 mb-4" href="./" data-aos="fade-up" data-aos-delay="150">
+                    <span>返回首頁</span>
+                </a>
+            </div>
+            <div class="bg position-absolute max960 w-100">
+                <img src="styles/images/404/bg.png" alt="生豐電力" data-aos="fade-up" data-aos-delay="200">
+            </div>
+        </main>
+        <!-- main ======================================================================================= -->
+
+    </div>
+    <?php include "footer-js.php"; ?>
+</body>
+
+</html>
