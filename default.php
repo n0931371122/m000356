5734145d278e0a8451c11ad80fd777be341db022
n0931371122 <n0931371122@gmail.com>
2021-09-15 14:55:02 +0800

    add default.php

---
+++ default.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="zh-Hant">
+
+<head>
+    <?php include "header-title.php"; ?>
+
+</head>
+
+<body>
+    <div class="wp colorBlackHeader">
+        <?php include "header.php"; ?>
+        <!-- main ======================================================================================= -->
+        <main class="main">
+
+        </main>
+        <!-- main ======================================================================================= -->
+        <?php include "footer.php"; ?>
+    </div>
+    <?php include "footer-js.php"; ?>
+</body>
+
+</html>
